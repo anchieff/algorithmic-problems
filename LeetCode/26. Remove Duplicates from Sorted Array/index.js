23fc415c79c40b4de86bf783d4aedc93ee968a2d
anchieff <info@annanekrasova.ru>
2022-10-03 09:36:41 +0400

    26. Remove Duplicates from Sorted Array

---
+++ LeetCode/26. Remove Duplicates from Sorted Array/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = (nums) => {
+  let k = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[k - 1] !== nums[i]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  nums.length = k;
+  return k;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
